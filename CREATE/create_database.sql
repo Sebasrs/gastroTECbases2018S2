81ba6690e46592957b6d11207ee49528be7dae22
Sebastian Rivera Soto <sebastianrs72@gmail.com>
2018-09-05 22:05:29 -0600

    Agrego select por fechas

---
+++ CREATE/create_database.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE GastroTEC;
+
+USE GastroTEC;
--- READ/select_platillo_favorito_semestre.sql
+++ READ/select_platillo_favorito_semestre.sql
@@ -4,7 +4,7 @@ INNER JOIN Platillo
 	ON VotoPlatillo.IdPlatillo = Platillo.IdPlatillo
 INNER JOIN TipoVoto
 	ON TipoVoto.IdTipoVoto = VotoPlatillo.IdTipoVoto
-WHERE Tipo LIKE 'Positivo' AND VotoPlatillo.Fecha BETWEEN '2018-09-05 00:00:00' AND DATETIME('2018-09-05 00:00:00', '+7 days')
+WHERE Tipo LIKE 'Positivo' AND VotoPlatillo.Fecha BETWEEN '2018-09-05 00:00:00' AND DATETIME('2018-09-05 00:00:00', '+6 month')
 GROUP BY Nombre
 ORDER BY Likes DESC
 LIMIT 1;
